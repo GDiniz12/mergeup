097670e0d6ae035726ce9e0498386f3140aaba5f
Gabriel Diniz <gabrielvmdiniz12@gmail.com>
2026-05-20 10:25:43 -0300

    Feature: room connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,9 +12,65 @@ const io = new Server(server, {
   }
 });
 
+interface PlayerRoom {
+  players: string[];
+  ready: { [key: string]: boolean };
+}
+
+const rooms: { [key: string]: PlayerRoom } = {};
+let waitingPlayer: string | null = null;
+
 io.on("connection", (socket) => {
+  console.log("Player connected:", socket.id);
+
+  // Find or create a room for the player
+  if (!waitingPlayer) {
+    waitingPlayer = socket.id;
+    socket.emit("waiting", { message: "Waiting for opponent..." });
+    console.log("Player waiting for opponent:", socket.id);
+  } else {
+    const roomId = `room-${waitingPlayer}-${socket.id}`;
+    rooms[roomId] = {
+      players: [waitingPlayer, socket.id],
+      ready: { [waitingPlayer]: false, [socket.id]: false }
+    };
+
+    io.to(waitingPlayer).emit("opponent_connected", { opponentId: socket.id });
+    socket.emit("opponent_connected", { opponentId: waitingPlayer });
+
+    io.to(waitingPlayer).socketsJoin(roomId);
+    socket.join(roomId);
+
+    console.log("Room created:", roomId);
+    waitingPlayer = null;
+  }
+
   socket.on("gameboard", (gameboard: number[][]) => {
-    console.log(gameboard);
+    const playerRoom = Object.values(rooms).find(room => room.players.includes(socket.id));
+    if (playerRoom) {
+      const opponentId = playerRoom.players.find(id => id !== socket.id);
+      if (opponentId) {
+        io.to(opponentId).emit("opponent_gameboard", { gameboard });
+      }
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Player disconnected:", socket.id);
+    
+    if (socket.id === waitingPlayer) {
+      waitingPlayer = null;
+    }
+
+    const roomId = Object.keys(rooms).find(id => rooms[id].players.includes(socket.id));
+    if (roomId) {
+      const playerRoom = rooms[roomId];
+      const opponentId = playerRoom.players.find(id => id !== socket.id);
+      if (opponentId) {
+        io.to(opponentId).emit("opponent_disconnected");
+      }
+      delete rooms[roomId];
+    }
   });
 });
 
